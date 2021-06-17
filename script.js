6e985cf3652c4f8a27bee3845259a65feedbce1a
Aigul <sharipova.aig@gmail.com>
2021-06-17 09:28:03 -0400

    script

--- script.js
+++ script.js
@@ -25,7 +25,18 @@ const personalMovieDB = {
   actors: {},
   genres: [],
   privat: false
-}
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = prompt('На сколько оцените его?', '');
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
 
 
 
